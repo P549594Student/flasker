0230e86139d8f1cce224dcc23ccbd81fd5dec4d9
Steve <s.harry.owen@gmail.com>
2025-08-05 13:35:27 +0100

    New Initial

---
+++ hello.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    first_name = "Harry"
+    stuff = "This is text"
+    favourite_pizza=["Pepperoni", "Cheese", "Meatball", "Hawaian", "Ham and Mushroom", 100]
+    
+    return render_template('index.html', first_name=first_name, stuff=stuff, favourite_pizza=favourite_pizza)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"),404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"),500
